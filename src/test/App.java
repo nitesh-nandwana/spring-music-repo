439ad672a2306feffed89def55324269f2ce1add
Nandwana <nitesh.nandwana@accenture.com>
2016-12-22 15:53:26 +0530

    adding spring-music-project

---
+++ src/test/App.java
@@ -0,0 +1,13 @@
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class App {
+	@Test
+	public void concatAndConvertString() {
+		System.out.println("in test");
+		assertEquals(0, 1);
+	}
+
+}
